0a9d1edea69ab19d4fc44105571e576809b89541
tino queija <martin.queija@wolox.com.ar>
2020-05-24 17:03:42 -0300

    fix(ElasticBeanstalk): pretty up build.sh and add version to health endpoint

--- build.sh
+++ build.sh
@@ -1,1 +1,10 @@
-zip -r build.zip Dockerfile package.json src .ebextensions Dockerrun.aws.json tsconfig.build.json tsconfig.json .env
+zip -r build.zip \
+  Dockerfile \
+  package.json \
+  src \
+  .ebextensions/https-instance.config \
+  .ebextensions/https-instance-single.config \
+  .ebextensions/create-dynamodb-table.config \
+  Dockerrun.aws.json \
+  tsconfig.build.json \
+  tsconfig.json
--- src/api/controllers/health.ts
+++ src/api/controllers/health.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { version } = require('../../../package.json');
 
 export function healthCheck(req: Request, res: Response): Response {
-  return res.send({ uptime: process.uptime() });
+  return res.send({ uptime: process.uptime(), version });
 }
