b00ac4cd1c1212ab21d39424c5ac69bd21a83daf
tino q <tinqueija@gmail.com>
2020-05-25 18:46:28 -0300

    chore(Minesweeper): lower default difficulty for demonstration purposes

--- src/constants/boards.ts
+++ src/constants/boards.ts
@@ -2,7 +2,7 @@ export const BOARD_DEFAULT_ROWS = 10;
 
 export const BOARD_DEFAULT_COLUMNS = 10;
 
-export const BOARD_DEFAULT_DIFFICULTY = 25;
+export const BOARD_DEFAULT_DIFFICULTY = 13;
 
 export const BOARD_MAX_ROWS = 20;
 
